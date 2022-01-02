a6aabd8830d9c28ffad38da69b8a2243063bdcb0
jvbaker95 <jvbaker95@hotmail.com>
2022-01-02 02:04:48 -0500

    Modified example.py

--- example.py
+++ example.py
@@ -1,13 +1,18 @@
 from spleeter.separator import Separator
-input_file = "inputs/02 She's So Mean.mp3"
 # file = Audio(input_file)
+files = [
+    "inputs/02 Gasoline.mp3",
+    "inputs/03 Give Me The Meltdown.mp3",
+    "inputs/05 Mockingbird.mp3"
+]
 
 separator = Separator(
     params_descriptor="configs/2stems/base_config.json",
     multiprocess=False
 )
 
-separator.separate_to_file(
-    audio_descriptor=input_file,
-    destination="outputs/"
-)
+for file in files:
+    separator.separate_to_file(
+        audio_descriptor=file,
+        destination="outputs/"
+    )
