920f61b728a43411816cb8197fbe3e68c109ca35
J Baker <jvbaker95@hotmail.com>
2023-11-16 19:33:23 -0500

    16 Nov 2023

--- example.py
+++ example.py
@@ -1,12 +1,14 @@
+# winget install ffmpeg
 import spleeter
+from spleeter import audio
 from spleeter.separator import Separator
 from spleeter.audio import Codec
 from pathlib import Path
-# file = Audio(input_file)
-files = [str(i) for i in Path("inputs").iterdir()]
-# files = [
-#     str(Path("inputs/12 Heart Of The World.mp3"))
-# ]
+# file = audio(input_file)
+# files = [str(i) for i in Path("inputs").iterdir()]
+files = [
+    str(Path("inputs/05 Girl Like That (LP Version).mp3"))
+]
 separator = Separator(
     params_descriptor="configs/2stems/base_config.json",
     multiprocess=False
